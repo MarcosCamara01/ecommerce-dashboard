6477866733d88d1866f2a27a9b13329708e7c497
Marcos Penelas <marcospenelascamara@gmail.com>
2024-03-29 01:59:56 +0000

    redirect main page

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,15 @@ const nextConfig = {
             },
         ],
     },
+    async redirects() {
+        return [
+          {
+            source: '/',
+            destination: '/orders',
+            permanent: true,
+          },
+        ]
+      },
 };
 
 export default nextConfig;
